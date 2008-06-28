5f5a716cfc2b7c170143d6fcecc2547fb1f835be
kchine <kchine@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-28 16:33:31 +0000

    SSH Tunneling for DB registry quering
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biocep@203 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src_rpf/uk/ac/ebi/microarray/pools/SSHTunnelingWorker.java
+++ src_rpf/uk/ac/ebi/microarray/pools/SSHTunnelingWorker.java
@@ -7,8 +7,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -20,71 +18,70 @@ import uk.ac.ebi.microarray.pools.db.ConnectionProvider;
 import uk.ac.ebi.microarray.pools.db.DBLayer;
 
 public class SSHTunnelingWorker {
-	
-	public static HashMap<String, Object> servantMap=new HashMap<String, Object>();
-	public static Object invoke(String servantName, String methodName, Class<?>[] methodSignature, Object[] methodParameters) throws SSHTunnelingException {		
+
+	public static HashMap<String, Object> servantMap = new HashMap<String, Object>();
+
+	public static Object invoke(String servantName, String methodName, Class<?>[] methodSignature, Object[] methodParameters) throws SSHTunnelingException {
 		Object servant = servantMap.get(servantName);
 		if (servant == null) {
 			throw new SSHTunnelingException("Bad Servant Name :" + servantName);
 		}
 		Method m = null;
 		try {
-			m=servant.getClass().getMethod(methodName, methodSignature);
+			m = servant.getClass().getMethod(methodName, methodSignature);
 		} catch (NoSuchMethodException e) {
 			throw new SSHTunnelingException("Bad Method Name :" + methodName);
 		}
-		Object result=null;
+		Object result = null;
 		try {
 			result = m.invoke(servant, methodParameters);
 		} catch (InvocationTargetException e) {
-			throw new SSHTunnelingException("",e.getCause());
+			throw new SSHTunnelingException("", e.getCause());
 		} catch (Exception e) {
-			throw new SSHTunnelingException("",e);
-		}		
+			throw new SSHTunnelingException("", e);
+		}
 		return result;
 	}
 
+	public static void init() throws Exception {
+		final String dbdriver = "org.apache.derby.jdbc.ClientDriver";
+		final String dburl = "jdbc:derby://127.0.0.1:1527/DWEP;create=true";
+		final String dbuser = "DWEP";
+		final String dbpassword = "DWEP";
+		Class.forName(dbdriver);
+		DBLayer registry = DBLayer.getLayer(getDBType(dburl), new ConnectionProvider() {
+			public Connection newConnection() throws java.sql.SQLException {
+				return DriverManager.getConnection(dburl, dbuser, dbpassword);
+			};
+		});
+		servantMap.put("derby", registry);
+	}
+
 	public static void main(String[] args) {
-		try {			
-			
-			final String dbdriver="org.apache.derby.jdbc.ClientDriver";
-			final String dburl="jdbc:derby://127.0.0.1:1527/DWEP;create=true";
-			final String dbuser="DWEP";
-			final String dbpassword="DWEP";		
-			
-			Class.forName(dbdriver);
-			DBLayer registry = DBLayer.getLayer(getDBType(dburl), new ConnectionProvider() {
-				public Connection newConnection() throws java.sql.SQLException {
-					return DriverManager.getConnection(dburl, dbuser, dbpassword);
-				};
-			});			
-			servantMap.put("derby", registry);		
-			
-			Properties invokationProps=new Properties();
-			String fileIn=args[0];
-			invokationProps.loadFromXML(new FileInputStream(fileIn));			
-			Object result=null;
-			
-			try {
-				result=invoke(invokationProps.getProperty("servantName"), invokationProps.getProperty("methodName"), 
-						(Class<?>[])PoolUtils.hexToObject(invokationProps.getProperty("methodSignature")), (Object[])PoolUtils.hexToObject(invokationProps.getProperty("methodParameters")) );
-			} catch (SSHTunnelingException e) {
-				result=e;
-			} catch (Exception e) {
-				result=new SSHTunnelingException("",e);
-			}			
-			
-			
-			Properties invokationResult=new Properties();
+		String fileIn = args[0];
+		Object result = null;
+		try {
+			init();
+			Properties invokationProps = new Properties();
+			invokationProps.loadFromXML(new FileInputStream(fileIn));
+			result = invoke(invokationProps.getProperty("servantName"), invokationProps.getProperty("methodName"), (Class<?>[]) PoolUtils
+					.hexToObject(invokationProps.getProperty("methodSignature")), (Object[]) PoolUtils.hexToObject(invokationProps
+					.getProperty("methodParameters")));
+		} catch (SSHTunnelingException e) {
+			result = e;
+		} catch (Exception e) {
+			result = new SSHTunnelingException("", e);
+		}
+
+		try {
+			Properties invokationResult = new Properties();
 			invokationResult.put("result", PoolUtils.objectToHex(result));
-			ByteArrayOutputStream baos=new ByteArrayOutputStream();
-			invokationResult.storeToXML(baos,"");						
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			invokationResult.storeToXML(baos, "");
 			System.out.println("->XML");
-			BufferedReader br=new BufferedReader(new InputStreamReader(new ByteArrayInputStream(baos.toByteArray())));
-			String line=null;
-			while ((line=br.readLine())!=null) System.out.println(line);
-			br.close();			
-			new File(fileIn).delete();			
+			BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(baos.toByteArray())));
+			String line = null;	while ((line = br.readLine()) != null) System.out.println(line);
+			new File(fileIn).delete();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
