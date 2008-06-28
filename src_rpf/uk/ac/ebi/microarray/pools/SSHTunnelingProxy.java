30b590090e5807aa90be4fcfd37f8651249d2b8a
kchine (Mr ) <karim.chine@gmail.com>
2008-06-28 15:07:56 +0000

    SSH Tunneling for DB registry quering
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biocep@200 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src_rpf/uk/ac/ebi/microarray/pools/SSHTunnelingProxy.java
+++ src_rpf/uk/ac/ebi/microarray/pools/SSHTunnelingProxy.java
@@ -5,36 +5,32 @@ import java.io.FileOutputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
 import java.util.Properties;
-import java.util.Random;
-
-import uk.ac.ebi.microarray.pools.db.DBLayerInterface;
+import java.util.UUID;
 
 public class SSHTunnelingProxy {
 	
-	static Random rnd=new Random(System.currentTimeMillis());
 	
-	public static Object invoke(String dir, String servantName, String methodName, Class<?>[] methodSignature, Object[] methodParameters) throws SSHTunnelingException {
+	public static Object invoke(String sshHostIp, String sshLogin, String sshPwd, String homedir, String invokeCommand, String servantName, String methodName, Class<?>[] methodSignature, Object[] methodParameters) throws SSHTunnelingException {
 		try {
-			Properties invokationPros=new Properties();
-			invokationPros.put("servantName", servantName);
-			invokationPros.put("methodName", methodName);
-			invokationPros.put("methodSignature", PoolUtils.objectToHex(methodSignature));
-			invokationPros.put("methodParameters", PoolUtils.objectToHex(methodParameters));
+			Properties invokationProps=new Properties();
+			invokationProps.put("servantName", servantName);
+			invokationProps.put("methodName", methodName);
+			invokationProps.put("methodSignature", PoolUtils.objectToHex(methodSignature));
+			invokationProps.put("methodParameters", PoolUtils.objectToHex(methodParameters));
+			String uid=UUID.randomUUID().toString();
 			
-			String fileIn=dir+"invoke"+rnd.nextInt(100000)+".in";
-			
+			String fileIn=System.getProperty("java.io.tmpdir")+"invoke"+uid+".in";			
 			FileOutputStream fos=new FileOutputStream(fileIn);
-			invokationPros.storeToXML(fos, "");
+			invokationProps.storeToXML(fos, "");
 			fos.close();
-						
-			SSHTunnelingWorker.main(new String[]{fileIn});
-						
-			String fileOut=fileIn.substring(0, fileIn.lastIndexOf("."))+".out";
 			
+			SSHUtils.putFileSsh(fileIn, homedir, sshHostIp, sshLogin, sshPwd);
+			SSHUtils.execSsh(invokeCommand, sshHostIp, sshLogin, sshPwd);
+			SSHUtils.getFileSsh(homedir+"/"+"invoke"+uid+".out", System.getProperty("java.io.tmpdir")+"invoke"+uid+".out", sshHostIp, sshLogin, sshPwd);
+						
 			Properties resultProps=new Properties();
-			resultProps.loadFromXML(new FileInputStream(fileOut));
+			resultProps.loadFromXML(new FileInputStream(System.getProperty("java.io.tmpdir")+"invoke"+uid+".out"));
 			Object result=PoolUtils.hexToObject(resultProps.getProperty("result"));
 			if (result instanceof SSHTunnelingException) throw (SSHTunnelingException)result;
 			else return result;
@@ -51,19 +47,13 @@ public class SSHTunnelingProxy {
 		}
 	}
 	
-	public static Object getDynamicProxy(final String dir, final String servantName, Class<?>[] c) {
+	public static Object getDynamicProxy(final String sshHostIp,final String sshLogin,final String sshPwd,final String homedir,final String invokeCommand, final String servantName, Class<?>[] c) {
 		Object proxy = Proxy.newProxyInstance(SSHTunnelingProxy.class.getClassLoader(), c, new InvocationHandler() {
 			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-				return SSHTunnelingProxy.invoke(dir, servantName, method.getName(), method.getParameterTypes(), args);
+				return SSHTunnelingProxy.invoke(sshHostIp,sshLogin, sshPwd,homedir,invokeCommand, servantName, method.getName(), method.getParameterTypes(), args);
 			}
 		});
 		return proxy;
 	}
 
-	public static void main(String[] args) throws Exception {
-		
-		DBLayerInterface dbLayer=(DBLayerInterface)getDynamicProxy("", "derby", new Class<?>[] {DBLayerInterface.class});		
-		String[] result=dbLayer.list();
-		System.out.println("result="+Arrays.toString(result));		
-	}
 }
