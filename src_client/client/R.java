291f47d87e846fb12a56abac7a61df211e48028f
kchine (Mr ) <karim.chine@gmail.com>
2008-08-19 16:11:51 +0000

    access the Naming Registry from  groovy/python
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biocep@312 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src_client/client/R.java
+++ src_client/client/R.java
@@ -5,13 +5,20 @@ import java.rmi.registry.Registry;
 import graphics.rmi.GDApplet;
 import remoting.RServices;
 import uk.ac.ebi.microarray.pools.ServerDefaults;
+import uk.ac.ebi.microarray.pools.gui.InDialog;
 
 public class R {
-	static public RServices getInstance() {
+	public static  RServices getInstance() {
 		if (GDApplet._instance==null) return null;
 		else return GDApplet._instance.getR();
 	}
-	static public Registry getRegistry() throws Exception {
+	public static  Registry getRegistry() throws Exception {
 		return ServerDefaults.getRmiRegistry();
 	}
+	public static String getUserInput(String label) {
+		InDialog dialog=new InDialog(null,label,new String[]{""});
+		dialog.setVisible(true);
+		return dialog.getExpr();
+	}
+	
 }
