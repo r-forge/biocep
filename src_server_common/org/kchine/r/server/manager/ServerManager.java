67959c9baa1ba2ffcb421d63b7632534bb91f7e7
kchine <kchine@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-25 17:12:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biocep@457 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src_server_common/org/kchine/r/server/manager/ServerManager.java
+++ src_server_common/org/kchine/r/server/manager/ServerManager.java
@@ -631,7 +631,7 @@ public class ServerManager {
 						env.put("LD_LIBRARY_PATH", SCI_HOME+"bin"+System.getProperty("path.separator")+env.get("LD_LIBRARY_PATH"));	
 					} else {
 						env.put("LD_LIBRARY_PATH", SCI_HOME+"lib/scilab"+System.getProperty("path.separator")+SCI_HOME+"lib/thirdparty"+System.getProperty("path.separator")+env.get("LD_LIBRARY_PATH"));
-						//env.put("SCI", SCI_HOME + "share/scilab");						
+						env.put("SCI", SCI_HOME + "share/scilab");						
 					}
 					
 					env.put("SCI_DISABLE_TK","1");
