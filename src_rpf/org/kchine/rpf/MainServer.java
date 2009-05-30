461ffcf35a98c8ed11f93613050eaf06b8f156c8
kchine (Mr ) <karim.chine@gmail.com>
2009-05-30 10:50:12 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biocep@486 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src_rpf/org/kchine/rpf/MainServer.java
+++ src_rpf/org/kchine/rpf/MainServer.java
@@ -29,8 +29,6 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Vector;
 
-import javasci.Scilab;
-
 import org.apache.commons.logging.Log;
 import org.kchine.rpf.ManagedServant;
 import org.kchine.rpf.PoolUtils;
@@ -62,11 +60,7 @@ public class MainServer {
 
 	public static void main(String[] args) throws Exception {
 
-		
-		for (int i=0;i<300;++i) Scilab.Exec("disp(22+1);");
-		
 
-		
 		PoolUtils.initLog4J();
 		PoolUtils.ensurePublicIPIsUsedForRMI();
 		PoolUtils.noGui();
